b9fa40461f2c7e7b7fb6657f20665e3f7df50c9d
ruby <sahasransub@gmail.com>
2021-04-14 10:11:34 -0700

    Add project files.

---
+++ WebApplication4/Models/ApplicationDbContet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class ApplicationDbContet : DbContext
+    {
+        public  ApplicationDbContet(DbContextOptions<ApplicationDbContet>options):base(options)
+            {
+
+            }
+        public DbSet<Student> Student { get; set; }
+    }
+}
